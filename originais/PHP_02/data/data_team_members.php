c2b4b59f0eb80381dac1677064a01c3d2aaee84a
Pedro Beça <pedrobeca@ua.pt>
2020-03-23 16:18:55 +0000

    Módulo 2 ‐ PHP | Exercício 2 – AVILA Crew Website Introdução à arquitetura de um website em PHP

---
+++ originais/PHP_02/data/data_team_members.php
@@ -0,0 +1,22 @@
+<?php
+$mentor1_id="5";
+$mentor1_name="Joana Beja";
+$mentor1_title="especial mentor";
+$mentor1_description_short="ex-NTC e Mestre em Design, a Joana não esconde que o seu piso preferido do CCCI é o do meio.";
+$mentor1_description="ex-NTC e Mestre em Design, a Joana não esconde que o seu piso preferido do CCCI é o do meio.<br>Se por um lado a comunicação lhe está no sangue, por outro é a programação que a desafia, e ajuda a liderar a AVILA Crew para ajudar no superar dos objetivos em Lab4-A.";
+$mentor1_image="specialmentor_joana.jpg";
+
+$mentor2_id="6";
+$mentor2_name="Josué Silvério";
+$mentor2_title="especial mentor";
+$mentor2_description_short="A frequentar o Mestrado em Comunição Multimédia, o Josué assume um especial fascínio pelo mundo dos videojogos. A título de curiosidade, é o desenvolvimento de jogos que mais a desafia apesar de assumir o seu péssimo jeito para os jogar!";
+$mentor2_description="A frequentar o Mestrado em Comunicação Multimédia, o Josué assume um especial fascínio pelo mundo dos videojogos. A título de curiosidade, é o desenvolvimento de jogos que mais a desafia apesar de assumir o seu péssimo jeito para os jogar!<br>Junta-se à AVILA Crew para partilhar este gosto e demonstrar que Lab4-A pode ser uma UC bem simpática.";
+$mentor2_image="specialmentor_josue.jpg";
+
+$mentor3_id="7";
+$mentor3_name="Rita Lemos";
+$mentor3_title="especial logística";
+$mentor3_description_short="A frequentar NTC, a Rita não adora programação mas adora logística e tudo o que possa ser construído manualmente.";
+$mentor3_description="A frequentar NTC, a Rita não adora programação mas adora logística e tudo o que possa ser construído manualmente.<br>Finalista de NTC, faz parte da AVILA Crew, sendo uma das pessoas que trabalha nos bastidores para que as maravilhosas ideias e guiões da equipa tomem forma no cenário mais apropriado.";
+$mentor3_image="speciallogistica_rita.jpg";
+
