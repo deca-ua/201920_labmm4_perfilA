a90b3fb71451e4e6d710bd4cad48bc1612883c2f
csantosnapraia <carlos.m.santos@gmail.com>
2020-05-02 11:54:53 +0100

    Exercício PHP_BD_03

---
+++ originais/PHP_MySQL_03a/public/components/cp_user_password_recover.php
@@ -0,0 +1,13 @@
+<section id="recover">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-8 mx-auto text-center">
+                <h2 class="section-heading">Recuperação de password</h2>
+                <hr class="my-4">
+                <p class="mb-5">Se não se recorda da sua password, introduza o seu email de registo e
+                    siga as instruções recebidas por email.</p>
+                <p class="mb-5">Nota: Para fazer mais tarde!</p>
+            </div>
+        </div>
+    </div>
+</section>
